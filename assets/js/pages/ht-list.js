f9097263446c212e72fbae805c6422e3cfc1135a
ENES COLAK <enescolak@cyh.local>
2021-12-17 16:04:19 +0300

    first commit

---
+++ assets/js/pages/ht-list.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".wrapper .swiper", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  pagination: {
+    el: ".wrapper .swiper .swiper-pagination",
+    dynamicBullets: true,
+  },
+});
